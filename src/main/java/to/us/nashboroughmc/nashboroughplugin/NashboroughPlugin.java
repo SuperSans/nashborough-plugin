af03a5ae8e4292dd98b1d83b88cedb36d4ca1c25
Jacob Muchow <jacobamuchow@gmail.com>
2015-02-15 22:01:28 -0600

    Original commit

---
+++ src/main/java/to/us/nashboroughmc/nashboroughplugin/NashboroughPlugin.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package to.us.nashboroughmc.nashboroughplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NashboroughPlugin extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        
+    }
+    
+    @Override
+    public void onDisable() {
+        
+    }
+}
